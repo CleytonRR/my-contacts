bcfcfdad4898f89cc02e0418f92a899d908c2702
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-02-26 20:40:44 -0300

    Install nodemon

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Hello World')
+})
+
+app.listen(3000, () => console.log('Server running at 3000'))
