16abc008b673ec93797c33f3b291a5888be25984
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-02-26 21:05:55 -0300

    chore: Add eslint and config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello World')
-})
+  response.send('Hello World');
+});
 
-app.listen(3000, () => console.log('Server running at 3000'))
+app.listen(3000, () => console.log('Server running at 3000'));
