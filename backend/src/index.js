861f14ad8eb786c46f3b10424a227c87081982b2
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-02-27 11:36:40 -0300

    refactor(routes): Move routes to own file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('Server running at 3000'));
