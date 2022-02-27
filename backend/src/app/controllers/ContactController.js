94da66586b8cafd4f7da4438ae20e9aafd5fff7a
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-02-27 11:54:38 -0300

    feat: Added method findAll from repository in controller

--- backend/src/app/controllers/ContactController.js
+++ backend/src/app/controllers/ContactController.js
@@ -1,6 +1,10 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
 class ContactController {
-  index(request, response) {
-    response.send('Hello');
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
   }
 
   show() {
