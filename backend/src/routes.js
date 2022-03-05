63c1110fa63a677b60c345fc0e4acc9ed65ba24c
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-05 17:20:20 -0300

    feat: Added categories endpoints

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const ContactController = require('./app/controllers/ContactController');
+const CategoriesController = require('./app/controllers/CategoriesController');
 
 const router = Router();
 
@@ -8,5 +9,8 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories/', CategoriesController.index);
+router.post('/categories/', CategoriesController.store);
 
 module.exports = router;
