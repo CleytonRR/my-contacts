40fc84d829c7e6f483bf288f2401551c246bc45d
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-05 16:25:29 -0300

    refactor: findById and findByEmail methods

--- backend/src/app/repositories/ContactsRepository.js
+++ backend/src/app/repositories/ContactsRepository.js
@@ -24,16 +24,16 @@ class ContactsRepository {
     return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+
+    return row;
   }
 
   async create({
