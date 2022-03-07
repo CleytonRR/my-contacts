f4e613445a8888d9269868d796ded0adad756fbb
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-07 20:37:54 -0300

    chore: Update rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'consistent-return': 'off',
     'no-promise-executor-return': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
