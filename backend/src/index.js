1b7cb4195dbaf0e653497ad9bd6d31811d8e1f88
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-07 20:38:22 -0300

    feat: added error handler

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,9 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server running at 3000'));
