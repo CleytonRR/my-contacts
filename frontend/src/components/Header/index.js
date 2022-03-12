3d8330699fe1d392d10afe50043d9f3e42e569ae
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-12 15:33:27 -0300

    refactor: create header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import { Container, InputSearchContainer } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts" width="201" />
+      <InputSearchContainer>
+        <input type="text" placeholder="pesquise pelo nome" />
+      </InputSearchContainer>
+    </Container>
+  );
+}
