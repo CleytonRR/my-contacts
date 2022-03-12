f6ea942e4c8860977b64db0d4826c4514e86f707
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-12 12:09:22 -0300

    feat: added default theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <h1>My contacts</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
