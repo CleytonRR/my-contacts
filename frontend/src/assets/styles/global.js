a76fa2698e7fb8dc6ae6af430da5b46282403edb
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-12 12:00:20 -0300

    feat: added reset css

---
+++ frontend/src/assets/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Sora', sans-serif;
+    font-size: 16px;
+  }
+
+  body {
+    background: #F6F5FC;
+  }
+`;
