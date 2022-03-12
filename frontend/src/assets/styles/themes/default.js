54686be5e0acfdb1255b201d6924df1074244cbb
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-12 15:57:48 -0300

    feat: added primary color

--- frontend/src/assets/styles/themes/default.js
+++ frontend/src/assets/styles/themes/default.js
@@ -1,5 +1,11 @@
 export default {
   colors: {
     background: '#F6F5FC',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5861FC',
+      dark: '#3346F8',
+    },
   },
 };
