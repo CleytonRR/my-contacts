8c850cb935ee9f6fc2ed3cc7e7787020f5381b68
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-12 16:05:28 -0300

    feat: added styles in ContactsList

--- frontend/src/components/ContactsList/index.js
+++ frontend/src/components/ContactsList/index.js
@@ -1,5 +1,12 @@
+import { Container, Header } from './styles';
+
 export default function ContactsList() {
   return (
-    <div>ContactsList</div>
+    <Container>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+    </Container>
   );
 }
