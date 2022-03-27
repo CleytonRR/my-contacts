ea057660e8aa5e0ff28bbd322d85ffa7acf8877d
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-03-27 18:50:24 -0300

    feat: added styles in contactList header

--- frontend/src/components/ContactsList/index.js
+++ frontend/src/components/ContactsList/index.js
@@ -1,4 +1,6 @@
-import { Container, Header } from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+
+import { Container, Header, ListContainer } from './styles';
 
 export default function ContactsList() {
   return (
@@ -7,6 +9,15 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>nome</span>
+            <img src={arrow} alt="Arrow icon" />
+          </button>
+        </header>
+      </ListContainer>
     </Container>
   );
 }
--- frontend/src/components/ContactsList/styles.js
+++ frontend/src/components/ContactsList/styles.js
@@ -29,3 +29,24 @@ export const Header = styled.header`
     }
   }
 `;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+
+    }
+  }
+
+`;
