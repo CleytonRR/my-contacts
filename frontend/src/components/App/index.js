025cb4890b58bf19c8f601027d619299b4bd39a6
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-25 23:48:49 -0300

    feat: Added Routes and BrowserRouter component

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,21 +1,24 @@
 import { ThemeProvider } from 'styled-components';
 
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
