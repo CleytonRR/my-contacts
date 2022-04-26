92e1f57af16bc2b2caa567e403d60f88a26cad6a
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-25 23:46:02 -0300

    feat: Added Pages in routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import NewContact from './pages/NewContact';
+import EditContact from './pages/EditContact';
+
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact />
-      <Route path="/new" />
-      <Route path="/edit/:id" />
+      <Route path="/" exact component={Home} />
+      <Route path="/new" component={NewContact} />
+      <Route path="/edit/:id" component={EditContact} />
     </Switch>
   );
 }
