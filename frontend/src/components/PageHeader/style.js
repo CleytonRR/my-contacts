21003dac11cd09a69744b8d0b4a312dbf80bafec
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-26 23:23:32 -0300

    feat: Added styled in page header

--- frontend/src/components/PageHeader/style.js
+++ frontend/src/components/PageHeader/style.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  a {
+    text-decoration: none;
+
+    span {
+      color: ${({ theme }) => theme.colors.primary.main};
+      font-weight: bold;
+    }
+
+    img {
+      margin-right: 8px;
+      transform: rotate(-90deg);
+    }
+  }
+
+  h1 {
+    font-size: 24px;
+  }
+`;
