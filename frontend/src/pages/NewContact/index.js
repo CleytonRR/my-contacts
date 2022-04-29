8a7d094385968543ba5b52f4e76e59eb3cb4e8e9
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-28 23:11:50 -0300

    feat: Added select input

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,11 +1,15 @@
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
       <Input />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
     </>
   );
 }
