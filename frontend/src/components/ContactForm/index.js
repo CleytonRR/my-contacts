42139153d78abdbccf448d5781172a793cac9be4
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-28 23:32:47 -0300

    feat: Added ContactForm

---
+++ frontend/src/components/ContactForm/index.js
@@ -0,0 +1,9 @@
+import { Form } from './styles';
+
+export default function ContactForm() {
+  return (
+    <Form>
+      form
+    </Form>
+  );
+}
--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,19 +1,11 @@
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
+import ContactForm from '../../components/ContactForm';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <Input />
-      <Select>
-        <option value="123">Instagram</option>
-      </Select>
-      <Button type="button">
-        Salvar alterações
-      </Button>
+      <ContactForm />
     </>
   );
 }
