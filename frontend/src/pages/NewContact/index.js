0ca4145136df282645fc21fbd82f3e506deda39c
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-28 23:06:37 -0300

    feat: Added input

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,7 +1,11 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input />
+    </>
   );
 }
