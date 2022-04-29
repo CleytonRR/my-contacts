cfdf3eed1ab0ade30a90c6a4039069c368bc4618
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-28 23:06:24 -0300

    feat: Added styled input component

---
+++ frontend/src/components/Input.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  background: #fff;
+  border: none;
+  border: 2px solid #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 4px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
