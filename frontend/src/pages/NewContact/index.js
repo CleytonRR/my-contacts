4b85665eb6d1bf92be7ed9f9d75d3e8b928ad5d2
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-28 23:24:02 -0300

    feat: Added buttton

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -10,6 +11,9 @@ export default function NewContact() {
       <Select>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
     </>
   );
 }
