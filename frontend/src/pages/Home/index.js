37730d40d2b514b0ed94b78d390b5885c12b5107
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-30 10:58:41 -0300

    feat: parse danger props to modal

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -13,7 +13,7 @@ import {
 export default function Home() {
   return (
     <Container>
-      <Modal />
+      <Modal danger />
       <InputSearchContainer>
         <input type="text" placeholder="pesquise pelo nome" />
       </InputSearchContainer>
