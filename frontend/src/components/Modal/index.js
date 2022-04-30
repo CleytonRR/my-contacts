635cb70d938e52deb92683d94488dbd95068bef2
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-30 10:58:54 -0300

    refactor: receiver danger prop

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,11 +1,12 @@
+import PropTypes from 'prop-types';
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Titulo do Modal</h1>
         <p>
           Corpo do modal
@@ -23,3 +24,11 @@ export default function Modal() {
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -23,6 +23,9 @@ export const Container = styled.div`
 
   h1 {
     font-size: 22px;
+    color: ${({ theme, danger }) => (
+    danger ? theme.colors.danger.main : theme.colors.gray[900]
+  )}
   }
 
   p {
