8439e8e3af9379530ce8e30f476fffa09ba14957
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-29 23:36:09 -0300

    feat: Added ContactForm

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,7 +1,11 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar Mateus Silva" />
+    <>
+      <PageHeader title="Editar Mateus Silva" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
   );
 }
