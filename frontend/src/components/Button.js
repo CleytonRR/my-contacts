f25681cb26804f69eec4e5bcec7a370be32b6ea8
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-30 10:52:15 -0300

    refactor: Remove width 100%

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.button`
-    width: 100%;
     height: 52px;
     border: none;
+    padding: 0 16px;
     background: ${({ theme }) => theme.colors.primary.main};
     font-size: 16px;
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.04);
