ec87aa6bc30da1029255656769fc2fca20d868d5
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-30 10:52:37 -0300

    feat: Added footer

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,11 +1,24 @@
-import { Overlay, Container } from './styles';
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
 
 export default function Modal() {
   return (
     <Overlay>
       <Container>
-        Modal
+        <h1>Titulo do Modal</h1>
+        <p>
+          Corpo do modal
+        </p>
 
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+          <Button type="button">
+            Deletar
+          </Button>
+        </Footer>
       </Container>
     </Overlay>
   );
