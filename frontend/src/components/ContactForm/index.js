7acb20e4ab1c367e9ebd6437a692f72cc4f74904
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-29 23:13:06 -0300

    feat: Added FormGroup

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,9 +1,10 @@
+import FormGroup from '../FormGroup';
 import { Form } from './styles';
 
 export default function ContactForm() {
   return (
     <Form>
-      form
+      <FormGroup />
     </Form>
   );
 }
---
+++ frontend/src/components/FormGroup/index.js
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export default function FormGroup() {
+  return (
+    <Container>
+      FKk
+    </Container>
+
+  );
+}
