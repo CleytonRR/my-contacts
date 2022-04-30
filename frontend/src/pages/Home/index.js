28e30c10b67c087d9d034171ce93e48f57d9bdd8
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-04-29 23:54:54 -0300

    feat: replace a to Link component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
@@ -14,7 +16,7 @@ export default function Home() {
       </InputSearchContainer>
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -36,54 +38,15 @@ export default function Home() {
           </div>
 
           <div className="actions">
-            <a href="/">
+            <Link to="/edit/123">
               <img src={edit} alt="Edit" />
-            </a>
+            </Link>
             <button type="button">
               <img src={trash} alt="Delete icon" />
             </button>
           </div>
         </Card>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Mateus Silva</strong>
-              <small>Instagram</small>
-            </div>
-            <span>mateus@devacademy.com.br</span>
-            <span>(41) 99999-999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete icon" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Mateus Silva</strong>
-              <small>Instagram</small>
-            </div>
-            <span>mateus@devacademy.com.br</span>
-            <span>(41) 99999-999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete icon" />
-            </button>
-          </div>
-        </Card>
       </ListContainer>
     </Container>
   );
