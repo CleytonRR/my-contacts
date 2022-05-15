a15090c6b0808593fe80120322420cc40a9c99e7
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-05-15 16:16:33 -0300

    feat: Added validation for email input

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { useState } from 'react';
+import isEmailValid from '../../utils/isEmailValid';
+
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -34,6 +36,27 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail inválido' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'email',
+      ));
+    }
+  }
+
   return (
     <Form onSubmit={(event) => handleSubmit(event)}>
       <FormGroup>
@@ -48,7 +71,10 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={(event) => {
+            setEmail(event.target.value);
+            handleEmailChange(event);
+          }}
         />
       </FormGroup>
 
