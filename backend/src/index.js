606d26d180651a0f38b8f984c8851a01e84aff07
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-05-19 22:24:41 -0300

    Added function to allow cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+});
+
 app.use(routes);
 
 app.use((error, request, response, next) => {
