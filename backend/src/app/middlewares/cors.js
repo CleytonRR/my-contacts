df616e098514646ab09f5710e2fae8fc9840f9bb
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-05-22 20:17:27 -0300

    feat: set allow methods

--- backend/src/app/middlewares/cors.js
+++ backend/src/app/middlewares/cors.js
@@ -1,4 +1,5 @@
 module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
   next();
 };
