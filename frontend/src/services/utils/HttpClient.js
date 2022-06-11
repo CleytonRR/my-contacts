96ce8fbc3b8c29cd344cc9912a72686dc3071dfc
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-06-10 23:00:24 -0300

    feat: added conditions by content-type

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -6,11 +6,19 @@ class HttpClient {
   async get(path) {
     const response = await fetch(`${this.baseURL}${path}`);
 
-    if (response.ok) {
-      return response.json();
+    let body = null;
+
+    const contentType = response.headers.get('Content-Type');
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(body.error);
   }
 }
 
