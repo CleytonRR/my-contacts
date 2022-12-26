82e62752e24e27cbbbb24181c0ec23fe625754e2
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-26 13:44:55 -0300

    feat: Added container to show message when users not have by filter

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -12,6 +12,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 
@@ -23,6 +24,7 @@ import {
   InputSearchContainer,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 
 export default function Home() {
@@ -127,6 +129,24 @@ export default function Home() {
             </p>
           </EmptyListContainer>
         )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+          <SearchNotFoundContainer>
+            <img src={magnifierQuestion} alt="Magnifier Question" />
+            <span>
+              Nenhum resultado foi encontrado para
+              <strong>
+                ”
+                {searchTerm}
+                ”
+              </strong>
+              .
+
+            </span>
+          </SearchNotFoundContainer>
+
+          )}
+
         {filteredContacts.length > 0 && (
         <ListHeader orderBy={orderBy}>
           <button type="button" onClick={handleToggleOrdeyBy}>
--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -158,3 +158,15 @@ export const EmptyListContainer = styled.div`
     }
   }
 `;
+
+export const SearchNotFoundContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  align-items: flex-start;
+
+  span {
+    margin-left: 24px;
+    color: ${({ theme }) => theme.colors.gray[200]};
+    word-break: break-word;
+  }
+`;
