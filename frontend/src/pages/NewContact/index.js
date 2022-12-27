4e5d45471639f43ffe735f3063bda92631557122
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-27 12:50:37 -0300

    feat: Added method to call api

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,11 +1,29 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log(response);
+    } catch {
+      alert('Erro ao cadastrar usuário');
+    }
+  }
+
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }
