a6ec6a7b6b03e72c0689acac22150299b6db3d77
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-27 12:49:44 -0300

    refactor: Create method to make request

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -5,22 +5,50 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
+  get(path, options) {
+    return this.makeRequest(path, { method: 'GET', headers: options?.headers });
+  }
 
-    let body = null;
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  async makeRequest(path, options) {
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([name, value]) => {
+        headers.append(name, value);
+      });
+    }
+
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: options.method,
+      body: JSON.stringify(options.body),
+      headers,
+    });
+
+    let responseBody = null;
 
     const contentType = response.headers.get('Content-Type');
 
     if (contentType.includes('application/json')) {
-      body = await response.json();
+      responseBody = await response.json();
     }
 
     if (response.ok) {
-      return body;
+      return responseBody;
     }
 
-    throw new APIError(body?.error || `${response.status} - ${response.statusText}`);
+    throw new APIError(responseBody?.error || `${response.status} - ${response.statusText}`);
   }
 }
 
