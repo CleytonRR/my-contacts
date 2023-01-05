8e7e079eccca420a8770636ae76030c521f6187d
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-05 16:02:59 -0300

    refactor: Added Map in class

--- frontend/src/lib/EventManager.js
+++ frontend/src/lib/EventManager.js
@@ -1,24 +1,36 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
+  }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove);
+
+    this.listeners[event] = filteredListeners;
   }
 }
 
