f0ed667bd06d4d1c0110673bca753f63e32f2cb4
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-06 16:40:21 -0300

    feat: Clear fileds after submit form

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -55,6 +55,10 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     });
 
     setIsSubmitting(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   function handleNameChange(event) {
