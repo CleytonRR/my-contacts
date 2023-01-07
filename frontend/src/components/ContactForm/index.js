1574f41b5503204372e606d4afa587d248778895
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-07 11:21:22 -0300

    feat: Added function to access handle with imperative form

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import {
+  useState, useEffect, forwardRef, useImperativeHandle,
+} from 'react';
 import PropTypes from 'prop-types';
 
 import CategoriesService from '../../services/CategoriesService';
@@ -13,7 +15,7 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-export default function ContactForm({ buttonLabel, onSubmit }) {
+const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -21,6 +23,9 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
   const [categories, setCategories] = useState([]);
   const [isLoadingCategories, setIsLoadingCategories] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const refObject = ref;
+  refObject.current = 'valor setado dentro do contact form';
 
   const {
     errors,
@@ -30,6 +35,15 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
   } = useErrors();
 
   const isFormValid = (name && errors.length === 0);
+
+  useImperativeHandle(ref, () => ({
+    setFieldsValue: (contact) => {
+      setName(contact.name);
+      setEmail(contact.email);
+      setPhone(contact.phone);
+      setCategoryId(contact.category_id);
+    },
+  }));
 
   useEffect(() => {
     async function loadCategories() {
@@ -142,9 +156,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       </ButtonContainer>
     </Form>
   );
-}
+});
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,11 +1,52 @@
+import { useEffect, useState, useRef } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import toast from '../../utils/toast';
+import ContactsService from '../../services/ContactsService';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
+import Loader from '../../components/Loader';
+
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFieldsValue(contact);
+
+        setIsLoading(false);
+      } catch (e) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Mateus Silva" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
