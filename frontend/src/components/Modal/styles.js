8264e9d16074170869b81bc05129c9c24dba91ae
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-07 11:50:37 -0300

    refactor: Change position absolute to fixed

--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
   backdrop-filter: blur(5px);
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
   left: 0;
