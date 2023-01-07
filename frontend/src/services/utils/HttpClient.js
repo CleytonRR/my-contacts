8296db83b1e3435547bbb10bc5a27c20969522c2
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-07 11:42:37 -0300

    feat: Added Put verb

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -17,6 +17,14 @@ class HttpClient {
     });
   }
 
+  put(path, options) {
+    return this.makeRequest(path, {
+      method: 'PUT',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
   async makeRequest(path, options) {
     const headers = new Headers();
 
