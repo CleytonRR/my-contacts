545db4665f01b2a4a00827b22d06d7fb3a7d12f6
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-09 09:29:52 -0300

    feat: Added fadeIn animation in modal

--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
@@ -11,6 +31,7 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +41,7 @@ export const Container = styled.div`
   border-radius: 4px;
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
