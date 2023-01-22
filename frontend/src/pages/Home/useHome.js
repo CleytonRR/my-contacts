338492d5552e07b83e6f0a128ce71ff587d1468a
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-01-22 13:36:06 -0300

    refactor: remove useless code

--- frontend/src/pages/Home/useHome.js
+++ frontend/src/pages/Home/useHome.js
@@ -62,7 +62,6 @@ export default function useHome() {
 
   function handleCloseDeleteModal() {
     setIsDeleteModalVisible(false);
-    setContactBeingDeleted(null);
   }
 
   async function handleConfirmDeleteContact() {
