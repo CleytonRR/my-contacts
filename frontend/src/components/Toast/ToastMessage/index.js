15374c47895bdf3809af622636cbe72505f95168
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-02-25 22:15:20 -0300

    feat: Added memo to optmize toast render

--- frontend/src/components/Toast/ToastMessage/index.js
+++ frontend/src/components/Toast/ToastMessage/index.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
-import { useEffect } from 'react';
+import { useEffect, memo } from 'react';
 import { Container } from './style';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({
+function ToastMessage({
   message, onRemoveMessage, isLeaving, animatedRef,
 }) {
   useEffect(() => {
@@ -49,3 +49,5 @@ ToastMessage.propTypes = {
   isLeaving: PropTypes.bool.isRequired,
   animatedRef: PropTypes.shape().isRequired,
 };
+
+export default memo(ToastMessage);
