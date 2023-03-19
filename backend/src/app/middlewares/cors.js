4b32308fdea104fcdee694c3d070132b2e7cacd0
Cleyton Furtado <cleytonrodrigues69@live.com>
2023-03-19 14:06:49 -0300

    feat: enable multi cors

--- backend/src/app/middlewares/cors.js
+++ backend/src/app/middlewares/cors.js
@@ -1,7 +1,18 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3002',
+  ];
+
+  const origin = request.header('Origin');
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
+
   next();
 };
